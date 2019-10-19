1b3d954cfdb74114e5043ec1532e1a27283b09d8
arpitprod <arpitg2@gmail.com>
2019-10-19 09:35:41 +0530

    Updates

---
+++ precache-manifest.9eff600f20ddc36fc47af25cba056407.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d149adb3176a7b44cb3c514c3d85c2c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae8d152904cc9d0d2d2e",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "ae8d152904cc9d0d2d2e",
+    "url": "/static/js/2.ad491eda.chunk.js"
+  },
+  {
+    "revision": "21fd908b3f0fb86b66dd",
+    "url": "/static/js/main.2bd54d22.chunk.js"
+  },
+  {
+    "revision": "a24e0db6d49bce10429c",
+    "url": "/static/js/runtime-main.4b4f0494.js"
+  }
+]);
