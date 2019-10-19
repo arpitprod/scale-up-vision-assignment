b670e7e453a9af633955e833dd88512939349008
arpitprod <arpitg2@gmail.com>
2019-10-19 09:41:13 +0530

    small change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 // Pages
 import RegisterFirst from './components/RegisterFirst';
